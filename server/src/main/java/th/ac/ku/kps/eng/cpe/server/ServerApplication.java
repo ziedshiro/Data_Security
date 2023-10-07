8e6036c49144f471b0123bf4d765f39bc755556d
Krirk KLOMKLIANG <krirk.k@ku.th>
2023-10-07 09:49:36 +0700

    new config

---
+++ server/src/main/java/th/ac/ku/kps/eng/cpe/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.kps.eng.cpe.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/th/ac/ku/kps/eng/cpe/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.kps.eng.cpe.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
