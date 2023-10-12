d62c72c22747366a6e71182d6bc3917d975e6f45
Krirk KLOMKLIANG <krirk.k@ku.th>
2023-10-13 01:30:06 +0700

    decode

--- server/src/main/java/th/ac/ku/kps/eng/cpe/service/DecryptionServices.java
+++ server/src/main/java/th/ac/ku/kps/eng/cpe/service/DecryptionServices.java
@@ -1,10 +1,25 @@
 package th.ac.ku.kps.eng.cpe.service;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.Security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,6 +46,24 @@ public class DecryptionServices {
 	    byte[] encryptedBytes = cipher.doFinal(data.getBytes());
 	    String encryptedData = Base64.getEncoder().encodeToString(encryptedBytes);
 	    return encryptedData;
+	}
+	public String Decode(String data) throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+		Security.addProvider(new BouncyCastleProvider());
+
+        byte[] encryptedDataBytes = Base64.getDecoder().decode(data);
+
+        byte[] iv = Base64.getDecoder().decode("16");
+
+        Cipher cipher = Cipher.getInstance("AES/CFB/PKCS5Padding", "BC");
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+
+        byte[] decryptedDataBytes = cipher.doFinal(encryptedDataBytes);
+
+        String decryptedData = new String(decryptedDataBytes, "UTF-8");
+        return decryptedData;
+
 	}
 	
 	
