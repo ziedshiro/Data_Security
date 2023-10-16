41ebed84b9405d233125462730d2a9815fbac229
Krirk KLOMKLIANG <krirk.k@ku.th>
2023-10-16 22:09:56 +0700

    generate_Pickup_QRCode

--- server/src/main/java/th/ac/ku/kps/eng/cpe/controller/PaymentController.java
+++ server/src/main/java/th/ac/ku/kps/eng/cpe/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package th.ac.ku.kps.eng.cpe.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pheerathach.ThaiQRPromptPay;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import io.jsonwebtoken.Claims;
 import th.ac.ku.kps.eng.cpe.auth.JwtUtil;
@@ -39,8 +45,8 @@ public class PaymentController {
 	@Autowired
 	private UserServices userservice;
 	
-	@PostMapping("auth/generateqrcode/{id}")
-	public QRCodeResponse generateQRCode(@RequestHeader("Authorization") String token,@PathVariable("id")String id) throws Exception {
+	@PostMapping("auth/generatepromptpayqrcode/{id}")
+	public QRCodeResponse generatePromptpayQRCode(@RequestHeader("Authorization") String token,@PathVariable("id")String id) throws Exception {
 		String jwtToken = token.replace("Bearer ", "");
 		Claims claims = jwtUtil.parseJwtClaims(jwtToken);
 		String username = (String) claims.get("username");
@@ -50,6 +56,33 @@ public class PaymentController {
 			Orders order = orderservice.findById(id);
 			ThaiQRPromptPay qrcode = new ThaiQRPromptPay.Builder().dynamicQR().creditTransfer().mobileNumber("0955523541").amount(order.getTotalAmount()).build();
 			reps.setResults("data:image/png;base64,"+qrcode.drawToBase64(300, 300));
+			reps.setMsg("create");
+			reps.setStatus(HttpStatus.CREATED);
+		}
+		else {
+			reps.setMsg("Unauthorized");
+			reps.setStatus(HttpStatus.UNAUTHORIZED);
+		}
+		return reps;
+	}
+	
+	@PostMapping("auth/generatepickupqrcode/{id}")
+	public QRCodeResponse generatePickupQRCode(@RequestHeader("Authorization") String token,@PathVariable("id")String id) throws Exception {
+		String jwtToken = token.replace("Bearer ", "");
+		Claims claims = jwtUtil.parseJwtClaims(jwtToken);
+		String username = (String) claims.get("username");
+		User user = userservice.findByUserId(encryptionservice.encrypt(username));
+		QRCodeResponse reps = new QRCodeResponse();
+		if (user != null) {
+			Orders order = orderservice.findById(id);
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = qrCodeWriter.encode(order.getPickupCode(), BarcodeFormat.QR_CODE, 300, 300);
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+            byte[] imageBytes = outputStream.toByteArray();
+            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+			
+			reps.setResults("data:image/png;base64,"+base64Image);
 			reps.setMsg("create");
 			reps.setStatus(HttpStatus.CREATED);
 		}
