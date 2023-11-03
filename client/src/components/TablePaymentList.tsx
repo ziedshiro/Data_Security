b5397249443a0c2c9c330b6a998a08c31d3eed85
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-11-03 10:38:34 +0700

    search

--- client/src/components/TablePaymentList.tsx
+++ client/src/components/TablePaymentList.tsx
@@ -6,8 +6,11 @@ import { useFetchPaymentQuery } from "../store";
 import { Skeleton } from "@mui/joy";
 import ModalApprovePayment from "./ModalApprovePayment";
 
+interface TablePayment {
+    search: string;
+}
 
-function TablePaymentList() {
+function TablePaymentList({search}:TablePayment) {
     const { data,isFetching  } = useFetchPaymentQuery('') as {
         data:Array<Payment>,
         isFetching:Boolean
@@ -23,35 +26,74 @@ function TablePaymentList() {
                 </td>
             </tr>
     }else if(data.length > 0){
-        content = data.map((item:Payment)=>{
+        const filterData = data.filter((item)=>{
             return(
-                <tr key={item.orderId}>
-                    <td className="px-3 text-start">
-                        {thaiDateFormat(item.orderDate)}
-                    </td>
-                    <td className="px-3">
-                        {thaiDateFormat(item.paymentDate)}
-                    </td>
-                    <td className="px-3">
-                        {item.totalAmount}
-                    </td>
-                    <td className="px-3">
-                        {item.orderStatus}
-                    </td>
-                    <td className="px-3">
-                        {item.paymentStatus}
-                    </td>
-                    <td className="px-3 py-2 flex">
-                        <EyeIcon onClick={()=>setViewPayment(true)} height={25} className="hover:text-yellow-600 cursor-pointer mr-1"/>
-                        <ModalApprovePayment 
-                            onHide={()=>setViewPayment(false)}
-                            open={viewPayment}
-                            payment={item}
-                        />
-                    </td>
-                </tr>
+                thaiDateFormat(item.paymentDate).includes(search) || thaiDateFormat(item.orderDate).includes(search)
             )
         })
+        console.log(filterData);
+        
+        if(search === ''){
+            content = data.map((item:Payment)=>{
+                return(
+                    <tr key={item.orderId}>
+                        <td className="px-3 text-start">
+                            {thaiDateFormat(item.orderDate)}
+                        </td>
+                        <td className="px-3">
+                            {thaiDateFormat(item.paymentDate)}
+                        </td>
+                        <td className="px-3">
+                            {item.totalAmount}
+                        </td>
+                        <td className="px-3">
+                            {item.orderStatus}
+                        </td>
+                        <td className="px-3">
+                            {item.paymentStatus}
+                        </td>
+                        <td className="px-3 py-2 flex">
+                            <EyeIcon onClick={()=>setViewPayment(true)} height={25} className="hover:text-yellow-600 cursor-pointer mr-1"/>
+                            <ModalApprovePayment 
+                                onHide={()=>setViewPayment(false)}
+                                open={viewPayment}
+                                payment={item}
+                            />
+                        </td>
+                    </tr>
+                )
+            })
+        }else{
+            content = filterData.map((item:Payment)=>{
+                return(
+                    <tr key={item.orderId}>
+                        <td className="px-3 text-start">
+                            {thaiDateFormat(item.orderDate)}
+                        </td>
+                        <td className="px-3">
+                            {thaiDateFormat(item.paymentDate)}
+                        </td>
+                        <td className="px-3">
+                            {item.totalAmount}
+                        </td>
+                        <td className="px-3">
+                            {item.orderStatus}
+                        </td>
+                        <td className="px-3">
+                            {item.paymentStatus}
+                        </td>
+                        <td className="px-3 py-2 flex">
+                            <EyeIcon onClick={()=>setViewPayment(true)} height={25} className="hover:text-yellow-600 cursor-pointer mr-1"/>
+                            <ModalApprovePayment 
+                                onHide={()=>setViewPayment(false)}
+                                open={viewPayment}
+                                payment={item}
+                            />
+                        </td>
+                    </tr>
+                )
+            })
+        }
     }else{
         content = 
             <tr>
--- client/src/pages/administrator/ManagePayment.tsx
+++ client/src/pages/administrator/ManagePayment.tsx
@@ -67,7 +67,7 @@ function ManagePayment() {
                                 type="text"
                                 id="form-subscribe-Filter"
                                 className="mr-4 rounded-lg border-transparent flex-1 appearance-none border border-gray-400 w-40 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-900 focus:border-transparent"
-                                placeholder="ค้นหา"
+                                placeholder="ค้นหา Date"
                                 onChange={(e)=>setSearch(e.target.value)}
                             />
                         </div>
@@ -115,7 +115,7 @@ function ManagePayment() {
                             </tr>
                         </thead>
                         <tbody className="border-black border-b-2">
-                            <TablePaymentList/>
+                            <TablePaymentList search={search}/>
                         </tbody>
                         </table>
                     </div>
